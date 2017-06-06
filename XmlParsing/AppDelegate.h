284bebde7603c055f2efebd7c69f6c93e5dedd3d
suren kumar <Surendra@surendra.local>
2017-06-06 12:16:07 +0530

    Initial Commit

---
+++ XmlParsing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XmlParsing
+//
+//  Created by Surendra on 6/6/17.
+//  Copyright © 2017 Surendra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XmlParsing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XmlParsing
+//
+//  Created by Surendra on 6/6/17.
+//  Copyright © 2017 Surendra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
